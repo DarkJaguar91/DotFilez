8f018acf38e543da5d57b0ad2c424744813edf39
Brandon Talbot <bjtal91@gmail.com>
2025-11-15 22:16:15 -0800

    fixes

--- install/install.sh
+++ install/install.sh
@@ -1,19 +1,21 @@
 #1/bin/sh
 
+echo "----------------------------------------------------------------"
 echo "Installing Paru"
 
 pacman --noconfirm -S paru
 
 echo "\n\nInstalling Niri & Noctalia-shell"
 
-paru --noconfirm -S niri noctalia-shell quickshell ttf-roboto inter-font gpu-screen-recorder brightnessctl ddcutil cliphist matugen-git cava wlsunset xdg-desktop-portal python3 polkit-kde-agent wl-clipboard xdg-desktop-portal-gtk xdg-desktop-portal-gnome
+paru --noconfirm -S niri noctalia-shell quickshell ttf-roboto inter-font gpu-screen-recorder brightnessctl ddcutil cliphist matugen-git cava wlsunset xdg-desktop-portal python3 polkit-kde-agent wl-clipboard xdg-desktop-portal-gtk xdg-desktop-portal-gnome neovim matugen foot
 
-echo "\n\nInstalling Tui Greeter"
+echo "----------------------------------------------------------------"
+echo "Installing Tui Greeter"
 
-paru --noconfirm -S tuigreet
+paru --noconfirm -S greetd-tuigreet
 systemctl enable greetd
 
-cat <<EOT >>/etc/greetd/config.toml
+cat <<EOT >/etc/greetd/config.toml
 [terminal]
 vt = 1
 
@@ -22,12 +24,15 @@ command = "tuigreet --cmd /bin/niri-session"
 user = "greeter"
 EOT
 
-echo "\n\nInstalling Flatpak and apps"
+echo "----------------------------------------------------------------"
+echo "Installing Flatpak and apps"
 
 paru --noconfirm -S flatpak
 
 flatpak install -y com.brave.Browser com.valvesoftware.Steam dev.vencord.Vesktop io.github.radiolamp.mangojuice com.spotify.Client com.vysp3r.ProtonPlus io.github.kolunmi.Bazaar org.freedesktop.Platform.VulkanLayer.MangoHud//25.08
 
-echo "\n\nCopying wallpapers"
+echo "----------------------------------------------------------------"
+echo "Copying wallpapers"
 
-cp -r ./Wallpapers $HOME/Pictures/Wallpapers
+mkdir -p ../Pictures
+cp -r ./Wallpapers ../Pictures/Wallpapers
